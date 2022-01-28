8e3c38e5ab81e6a6a06ddf992ec7094edf5b0228
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2022-01-27 23:05:13 -0500

    loginPage rewrite

---
+++ src/components/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import App from '../App';
+
+class LoginPage extends React.Component{
+    render(){
+
+    };
+}
+
+  export default LoginPage;
