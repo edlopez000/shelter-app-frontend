fafe2520aaad3fc50053826869c32d61f42d3342
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-02-09 19:02:50 -0500

    Wrapped App.js in BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
-// import LoginPage from './components/LoginPage.js';
-import QuickLinks from './components/QuickLinks.js';
+import LoginPage from './components/LoginPage.js';
+// import QuickLinks from './components/QuickLinks.js';
 import ButtonAppBar from './components/AppBar.js';
 import { Container } from '@mui/material';
 // import QuickLinks from './components/QuickList';
@@ -10,7 +10,8 @@ function App() {
   return (
     <Container maxWidth="sm">
       <ButtonAppBar />
-      < QuickLinks />
+      <LoginPage />
+      {/* < QuickLinks /> */}
     </Container>
   );
 }
