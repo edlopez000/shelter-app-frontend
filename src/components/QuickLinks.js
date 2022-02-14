58ffaed618d017608112f5f42039cd9a2e86c3fc
Steph Peets <stephanie.peets@gmail.com>
2022-02-14 03:00:00 -0500

    reconfigured material components

--- src/components/QuickLinks.js
+++ src/components/QuickLinks.js
@@ -1,88 +1,78 @@
 import * as React from "react";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import IconButton from "@mui/material/IconButton";
-import Divider from "@mui/material/Divider";
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import UserGreeting from "./quicklinks/UserGreeting";
-import AnimalIcon from "./quicklinks/AnimalIcon";
-import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
+import AnimalIcon from "../assets/AnimalIcon";
+import CleaningServicesIcon from "@mui/icons-material/CleaningServices";
+
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
 
 export default function QuickLinks() {
   return (
-    <div>
-      <List sx={{ width: "100%", maxWidth: 600, bgcolor: "background.paper" }}>
+ <>
         <UserGreeting />
-        <ListItem alignItems="flex-start">
-          <ListItem
-            secondaryAction={
-              <IconButton edge="end" aria-label="arrow">
-                <ArrowForwardIosSharpIcon />
-              </IconButton>
-            }
-          >
-            <ListItemIcon>
-              <AnimalIcon name="dog" />
-            </ListItemIcon>
 
-            <ListItemText
-              primary="Dogs"
-              secondary="Handle dog needs!"
-              sx={{ ml: 2 }}
-            />
+        <Typography
+          textAlign={"left"}
+          letterSpacing={0.15}
+          fontSize={19}
+          fontWeight={"bold"}
+        >
+          Quick Links
+        </Typography>
+
+        <List sx={{ width: "100%", maxWidth: 600 }}>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <AnimalIcon name="dog" />
+              </ListItemIcon>
+              <ListItemText primary="Dogs" secondary="Handle dog needs!" />
+              <ArrowForwardIosSharpIcon fontSize="small" />
+            </ListItemButton>
           </ListItem>
-        </ListItem>
 
-        <Divider component="li" sx={{ ml: 13, maxWidth: 425 }} />
-        <ListItem alignItems="flex-start">
-          <ListItem
-            secondaryAction={
-              <IconButton edge="end" aria-label="arrow">
-                <ArrowForwardIosSharpIcon />
-              </IconButton>
-            }
-          >
-            <ListItemIcon>
-              <AnimalIcon name="cat" />
-            </ListItemIcon>
+          <Divider />
 
-            <ListItemText
-              primary="Cats"
-              secondary="Handle cat needs!"
-              sx={{ ml: 2 }}
-            />
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <AnimalIcon name="cat" />
+              </ListItemIcon>
+              <ListItemText primary="Cats" secondary="Handle cat needs!" />
+              <ArrowForwardIosSharpIcon fontSize="small" />
+            </ListItemButton>
           </ListItem>
-        </ListItem>
 
-        <Divider component="li" sx={{ ml: 13, maxWidth: 425 }} />
-        <ListItem alignItems="flex-start">
-          <ListItem
-            secondaryAction={
-              <IconButton edge="end" aria-label="arrow">
-                <ArrowForwardIosSharpIcon />
-              </IconButton>
-            }
-          >
-            <ListItemIcon>
-              <CleaningServicesIcon 
-              htmlColor="black"
-              fontSize="large"
-              name="cleaning services" />
-            </ListItemIcon>
+          <Divider />
 
-            <ListItemText
-              primary="Housekeeping / Laundry"
-              secondary="Handle housekeeping needs!"
-              sx={{ ml: 2 }}
-            />
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <CleaningServicesIcon
+                  htmlColor="black"
+                  fontSize="large"
+                  name="cleaning services"
+                />
+              </ListItemIcon>
+              <ListItemText
+                primary="Housekeeping / Laundry"
+                secondary="Handle housekeeping needs!"
+              />
+              <ArrowForwardIosSharpIcon fontSize="small" />
+            </ListItemButton>
           </ListItem>
-        </ListItem>
-
-        <Divider component="li" sx={{ ml: 13, maxWidth: 425 }} />
+       
+        <Divider /> 
         
-      </List>
-    </div>
+        </List>
+    </>
   );
 }
