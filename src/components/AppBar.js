807e35a84adc906cc5e5bcb847ae279d75174b4c
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-02-24 17:11:26 -0500

    Ran prettier on AppBar.js

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -8,18 +8,21 @@ import MenuButton from './MenuButton';
 
 export default function ButtonAppBar() {
   return (
-    <Box sx={{ 
-      flexGrow: 1, marginBottom:10
-     }}>
-      <AppBar >
-        <Toolbar variant="dense" sx={{backgroundColor: 'darkblue'}}>
+    <Box
+      sx={{
+        flexGrow: 1,
+        marginBottom: 10,
+      }}
+    >
+      <AppBar>
+        <Toolbar variant="dense" sx={{ backgroundColor: 'darkblue' }}>
           <MenuButton />
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1 }}
-          >Home</Typography>
-          <Button color="inherit" sx={{'font-weight': 'bold'}}>Donate</Button>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Home
+          </Typography>
+          <Button color="inherit" sx={{ 'font-weight': 'bold' }}>
+            Donate
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
