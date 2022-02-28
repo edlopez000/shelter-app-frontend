69964ce01aeb6b7afb3548c6c44295343157a5c5
Peach Paulison <dlpeachpaulison@gmail.com>
2022-02-27 23:12:14 -0500

    404 Not Found

---
+++ src/components/NotFoundPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+    return (
+        <div id="wrapper">
+            <img src="https://imgur.com/aU6INii" />
+            <div id="info">
+                <h3>Nobody's purrrrfect!  This page could not be found</h3>
+            </div>
+        </div >
+    )
+}
+
+export default NotFoundPage
