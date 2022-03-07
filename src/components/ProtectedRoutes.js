99fae0f608f87ecd9f60e472ac40e2467531b30b
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-03-07 16:25:32 -0500

    ProtectedRoutes is working again

--- src/components/ProtectedRoutes.js
+++ src/components/ProtectedRoutes.js
@@ -6,7 +6,6 @@ import { UserContext } from './UserContext';
 export default function ProtectedRoutes() {
   const { user } = useContext(UserContext);
 
-  // const isAuth = user.auth;
-  const isAuth = true;
+  const isAuth = user.auth;
   return isAuth ? <Outlet /> : <LoginPage unauthed={true} />;
 }
