ce9fddf901a76c0da57ad54d4c988ec3ac181742
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-03-07 16:46:17 -0500

    Volunteer firstName, lastName, id are set in the UserContext

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import "./App.css";
-import LoginPage from "./components/LoginPage.js";
-import ButtonAppBar from "./components/AppBar.js";
-import { Container } from "@mui/material";
-import QuickLinks from "./components/QuickLinks";
-import { Routes, Route } from "react-router-dom";
-import { SelectDog } from "./components/SelectDog";
-import ProtectedRoutes from "./components/ProtectedRoutes";
-import { UserContext } from "./components/UserContext";
-import { useState } from "react";
-import SelectCat from "./components/SelectCat";
-import DogActivities from "./components/DogActivities";
-import CatActivities from "./components/CatActivities";
-import Housekeeping from "./components/Housekeeping";
-import NotFoundPage from "./components/NotFoundPage";
-import ResourceLinks from "./components/ResourceLinks";
-import AboutDev from "./components/AboutDev";
-import AboutTheApp from "./components/AboutTheApp";
+import './App.css';
+import LoginPage from './components/LoginPage.js';
+import ButtonAppBar from './components/AppBar.js';
+import { Container } from '@mui/material';
+import QuickLinks from './components/QuickLinks';
+import { Routes, Route } from 'react-router-dom';
+import { SelectDog } from './components/SelectDog';
+import ProtectedRoutes from './components/ProtectedRoutes';
+import { UserContext } from './components/UserContext';
+import { useState } from 'react';
+import SelectCat from './components/SelectCat';
+import DogActivities from './components/DogActivities';
+import CatActivities from './components/CatActivities';
+import Housekeeping from './components/Housekeeping';
+import NotFoundPage from './components/NotFoundPage';
+import ResourceLinks from './components/ResourceLinks';
+import AboutDev from './components/AboutDev';
+import AboutTheApp from './components/AboutTheApp';
 
 function App() {
-  const [user, setUser] = useState({ auth: false });
+  const [user, setUser] = useState({
+    auth: false,
+    firstName: '',
+    lastName: '',
+    volunteerID: null,
+  });
   return (
     <Container maxWidth="sm" sx={{ marginTop: 15 }}>
       <ButtonAppBar />
@@ -33,7 +38,7 @@ function App() {
             <Route path="dogActivities" element={<DogActivities />} />
             <Route path="catActivities" element={<CatActivities />} />
             <Route path="tasks" element={<Housekeeping />} />
-            <Route path="links" element={<ResourceLinks/>} />
+            <Route path="links" element={<ResourceLinks />} />
             <Route path="devs" element={<AboutDev />} />
             <Route path="about" element={<AboutTheApp />} />
           </Route>
